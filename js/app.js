6fcbe4a9d86dc993c88d6655aaffff0ec5022f3a
Alexander White <parzivalwhite@gmail.com>
2019-01-10 13:02:05 -0800

    question answer alerts

--- js/app.js
+++ js/app.js
@@ -7,69 +7,87 @@ var name = prompt('What is your name?');
 console.log('user name', name);
 document.getElementById('name').innerHTML = name;
 
+// Question 1
+
  var age = prompt('First Question! How old am I?');
  console.log('my age:', age);
 
  if (age === '24' || age === 'Twenty four') {
-     document.getElementById('one').innerHTML = 'Thats correct!';
+     alert('You are correct');
+     document.getElementById('one').innerHTML = 'Your Answer: ' + age;
      document.getElementById('one').style.color = 'green';
      count++;
  } else {
-     document.getElementById('one').innerHTML = 'Thats wrong!';
+    alert('Wrong! I am 24');
+     document.getElementById('one').innerHTML = 'Your Answer: ' + age;
      document.getElementById('one').style.color = 'red';
  }
+
+// Question 2
 
  var pizza = prompt('Second Question! Do I like pineapples on my pizza?').toUpperCase();
     console.log('pizza:', pizza);
 
  if (pizza === 'NO' || pizza === 'N') {
-     document.getElementById('two').innerHTML = 'Thats correct!';
+    alert('You are correct');
+     document.getElementById('two').innerHTML = 'Your Answer: ' + pizza;
      document.getElementById('two').style.color = 'green';
      count++;
  } else {
-     document.getElementById('two').innerHTML = 'Thats wrong!';
+    alert('Wrong! That is gross');
+     document.getElementById('two').innerHTML = 'Your Answer: ' + pizza;
      document.getElementById('two').style.color = 'red';
  }
+
+// Question 3
 
 var countries = prompt('Third Question! How many countries do you think I have been to?');
 console.log('number of countries:', countries);
 
-if (parseInt(countries) >= 40 && parseInt(countries) < 45 || parseInt(countries) > 45 && parseInt(countries) <= 50) {
-    document.getElementById('three').innerHTML = 'Thats close!';
-} 
-else if (countries === '45') {
-    document.getElementById('three').innerHTML = 'Thats correct!';
+if (countries === '45') {
+    alert('You are correct');
+    document.getElementById('three').innerHTML = 'Your Answer: ' + countries;
     document.getElementById('three').style.color = 'green';
     count++;
  } else {
-    document.getElementById('three').innerHTML = 'Thats wrong!';
+    alert('Wrong! I have been to 45 countries');
+    document.getElementById('three').innerHTML = 'Your Answer: ' + countries;
     document.getElementById('three').style.color = 'red';
 }
+
+// Question 4
 
  var moto = prompt('Fourth Question! Do I drive a motorcycle?').toUpperCase();
     console.log ('motorcycle', moto);
 
  if (moto === 'YES' || moto === 'Y') {
-     document.getElementById('four').innerHTML = 'Thats correct!';
+    alert('You are correct! His name is Dulahan');
+     document.getElementById('four').innerHTML = 'Your Answer: ' + moto;
      document.getElementById('four').style.color = 'green';
      count++;
  } else {
-     document.getElementById('four').innerHTML = 'Thats wrong!';
+    alert('Wrong! I love my motorcycle');
+     document.getElementById('four').innerHTML = 'Your Answer: ' + moto;
      document.getElementById('four').style.color = 'red';
  }
+
+// Question 5
 
  var last = prompt('Fifth Question! Would you like to know more?').toUpperCase();
     console.log ('more', last);
 
  if (last === 'YES' || last === 'Y') {
-     document.getElementById('five').innerHTML = 'Well then strap in!';
+    alert('Well then strap in!');
+     document.getElementById('five').innerHTML = 'Your Answer: ' + last;
      document.getElementById('five').style.color = 'green';
      count++;
  } else {
-     document.getElementById('five').innerHTML ='Too bad! There is more!';
+     alert('Too bad! There is more')
+     document.getElementById('five').innerHTML = 'Your Answer: ' + last;
      document.getElementById('five').style.color = 'red';
  }
 
+// Question 6
 
     var min=1; 
     var max=10;  
@@ -93,11 +111,9 @@ for (var i = 4; i > 0 ; i--) {
     }
 }
 
+// Question 7
 
-
-
-
-var answ = ['apple', 'banana', 'pear', 'orange']
+var answ = ['apples', 'bananas', 'pears', 'oranges']
 
 for (var i = 6; i > 0 ; i--) {
     var y = prompt('What kind of fruit do I like? You have '+ i +' attempts left.').toLowerCase();
